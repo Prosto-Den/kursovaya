aeca0144ed00fdfdc2dd5641e63e344d8804d9d1
Prost0_Den <comp1252@mail.ru>
2024-05-20 23:12:17 +0300

    Added windows for additing info about materials, clients and providers

--- add_window.py
+++ add_window.py
@@ -7,9 +7,9 @@ from PIL import Image, ImageTk
 import os
 
 
-class AddWindow(ctk.CTkToplevel):
-    def __init__(self, type: str):
-        super().__init__()
+class AddMaterialWindow(ctk.CTkToplevel):
+    def __init__(self, parent, type: str) -> None:
+        super().__init__(master = parent)
 
         self.title('Add book')
         self.geometry('400x600')
@@ -19,45 +19,58 @@ class AddWindow(ctk.CTkToplevel):
         # load default picture
         picture = Image.open('./pictures/books/default.png').resize((300, 300))
         self.__book_image = ImageTk.PhotoImage(picture)
-        self.__picture_name = 'default.png'
+        self.__picture_name: str = 'default.png'
 
         # widget for showing the picture
+        self.__title = ctk.CTkEntry(self, placeholder_text = 'Название', font = ENTRY_FONT)
         self.__image = ctk.CTkLabel(self, text = '', image = self.__book_image)
-
-        self.__picture_path = './pictures/books/'
+        self.__amount = ctk.CTkEntry(self, placeholder_text = 'Количество', font = ENTRY_FONT)
 
         self.focus()
         self.grab_set()
 
         self.__create_layout(type)
 
-        self.mainloop()
-
-    def __init_book_window(self):
-        # entry widgets
-        self.__title = ctk.CTkEntry(self, placeholder_text = 'Название', font = ENTRY_FONT)
+    # init widgets for book
+    def __init_book_window(self) -> None:
         self.__authors = ctk.CTkEntry(self, placeholder_text = 'Автор(ы)', font = ENTRY_FONT)
         self.__publisher = ctk.CTkEntry(self, placeholder_text = 'Издательство', font = ENTRY_FONT)
         self.__publish_year = ctk.CTkEntry(self, placeholder_text = 'Год издания', font = ENTRY_FONT)
         self.__amount = ctk.CTkEntry(self, placeholder_text = 'Количество', font = ENTRY_FONT)
 
-    def __create_book_layout(self):
+    # init widgets for paper
+    def __init_paper_window(self) -> None:
+        self.__number = ctk.CTkEntry(self, placeholder_text = 'Номер выпуска', font = ENTRY_FONT)
+        self.__date = ctk.CTkEntry(self, placeholder_text = 'Дата выпуска', font = ENTRY_FONT)
+
+    # init widgets for magazine
+    def __init_magazine_window(self):
+        self.__init_paper_window()
+
+        self.__publisher = ctk.CTkEntry(self, placeholder_text = 'Издательство', font = ENTRY_FONT)
+
+    # create layput for book
+    def __create_book_layout(self) -> None:
         self.__init_book_window()
 
-        self.__title.pack(fill = 'x', padx = 5, pady = 5)
         self.__authors.pack(fill = 'x', padx = 5, pady = 5)
         self.__publisher.pack(fill = 'x', padx = 5, pady = 5)
         self.__publish_year.pack(fill = 'x', padx = 5, pady = 5)
-        self.__amount.pack(fill = 'x', padx = 5, pady = 5)
 
-        ctk.CTkButton(self, 
-                  text = 'Сохранить', 
-                  font = FONT,
-                  text_color = BTN_TEXT_COLOUR,
-                  fg_color = BTN_COLOUR,
-                  hover_color = HOVER_BTN_COLOUR, 
-                  command = self.__save_book_data).pack(pady = 10)
+    # create layout for paper
+    def __create_paper_layout(self):
+        self.__init_paper_window()
 
+        self.__number.pack(fill = 'x', padx = 5, pady = 5)
+        self.__date.pack(fill = 'x', padx = 5, pady = 5)
+
+    def __create_magazine_layout(self):
+        self.__init_magazine_window()
+        self.__create_paper_layout()
+
+        self.__publisher.pack(fill = 'x', pady = 5, padx = 5)
+
+    # create the whole layout
     def __create_layout(self, type: str) -> None:
         self.__image.pack(pady = 5)
 
@@ -69,12 +82,30 @@ class AddWindow(ctk.CTkToplevel):
                   hover_color = HOVER_BTN_COLOUR, 
                   command = self.__select_image).pack(pady = 5)
         
+        self.__title.pack(fill = 'x', pady = 5, padx = 5)
+
         if type == 'Книгу':
             self.__create_book_layout()
 
+        elif type == 'Газету':
+            self.__create_paper_layout()
+
+        elif type == 'Журнал':
+            self.__create_magazine_layout()
+
+        self.__amount.pack(fill = 'x', pady = 5, padx = 5)
+
+        ctk.CTkButton(self, 
+                  text = 'Сохранить', 
+                  font = FONT,
+                  text_color = BTN_TEXT_COLOUR,
+                  fg_color = BTN_COLOUR,
+                  hover_color = HOVER_BTN_COLOUR, 
+                  command = self.__save_book_data).pack(pady = 10)
+
 
     # select image for the book
-    def __select_image(self):
+    def __select_image(self) -> None:
         file_path = askopenfile(filetypes = [('JPG files', ['*jpeg', '*jpg']), ('PNG files', '*png'), ('Image files', ['*jpg', '*jpeg', '*png'])])
 
         if file_path is None:
@@ -83,7 +114,7 @@ class AddWindow(ctk.CTkToplevel):
             self.__picture_name = file_path.name.split('/')[-1]
 
             # если файл с таким названием уже есть в папке - спрашиваем, нужно ли его копировать
-            if self.__picture_name in os.listdir(self.__picture_path):
+            if self.__picture_name in os.listdir(MATERIALS_PICTURE_PATH):
 
                 message = CTkMessagebox(self, title = 'Внимание',
                                         message = 'Файл с таким названием уже есть в папке. Заменить его?',
@@ -92,19 +123,27 @@ class AddWindow(ctk.CTkToplevel):
                                         icon = 'warning')
                 
                 if message.get() == 'Да':
-                    shutil.copy(file_path.name, self.__picture_path)
+                    shutil.copy(file_path.name, MATERIALS_PICTURE_PATH)
 
             else:
-                shutil.copy(file_path.name, self.__picture_path)
+                shutil.copy(file_path.name, MATERIALS_PICTURE_PATH)
 
-            path = self.__picture_path + self.__picture_name
+            path = MATERIALS_PICTURE_PATH + self.__picture_name
 
             image = Image.open(path).resize((300, 300))
             self.__book_image = ImageTk.PhotoImage(image)
 
             self.__image.configure(image = self.__book_image)
 
-    def __save_book_data(self):
+    def __show_warning(self) -> None:
+        CTkMessagebox(self, 
+                          title = 'Внимание',
+                          message = 'Необходимо заполнить все поля',
+                          icon = 'cancel',
+                          option_1 = 'OK',
+                          font = FONT)
+
+    def __save_book_data(self) -> None:
         title = self.__title.get()
         authors = self.__authors.get().split(',')
         publisher = self.__publisher.get()
@@ -112,15 +151,10 @@ class AddWindow(ctk.CTkToplevel):
         amount = self.__amount.get()
 
         if title == '' or authors == '' or publisher == '' or publish_year == 0 or amount == '':
-            CTkMessagebox(self, 
-                          title = 'Внимание',
-                          message = 'Необходимо заполнить все поля',
-                          icon = 'cancel',
-                          option_1 = 'Закрыть',
-                          font = FONT)
+            self.__show_warning()
             return
 
-        print(self.__picture_path + self.__picture_name)
+        print(MATERIALS_PICTURE_PATH + self.__picture_name)
         print(title)
         print(authors)
         print(publisher)
